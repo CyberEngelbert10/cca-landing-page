74fc6b07e72bf27fa9ffb4e9500ac47853c8f037
CyberEngelbert10 <malamboengelbert@gmail.com>
2025-04-14 11:17:11 +0200

    update for cloudflare

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,8 @@ const nextConfig = {
   images: {
     unoptimized: true, // Cloudflare Pages does not support Image Optimization API yet
   },
+  assetPrefix: 'https://copperbeltchessacademy.com',
+  basePath: '', // No additional base path needed for the root domain
 };
 
 export default nextConfig;
