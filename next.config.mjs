cef0cb91727bdae4b8f28bf1f3489d654ffc9f6d
CyberEngelbert10 <malamboengelbert@gmail.com>
2025-04-15 23:04:47 +0200

    update edge - runtime

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Optimize for Cloudflare Pages deployment
-  swcMinify: true,
   images: {
     unoptimized: true, // Cloudflare Pages does not support Image Optimization API yet
   },
   assetPrefix: 'https://copperbeltchessacademy.com',
   basePath: '', // No additional base path needed for the root domain
+  output: 'export', // Enable static export for Cloudflare Pages
 };
+
+export const runtime = "edge";
 
 export default nextConfig;
