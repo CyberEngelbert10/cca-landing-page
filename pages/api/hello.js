4689aa48bb78df98523c95bfcfc2ba15e98e6c20
CyberEngelbert10 <malamboengelbert@gmail.com>
2025-04-16 02:05:56 +0200

    update edge - runtime 3

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 export const runtime = 'edge';
 
+// This function handles the request and response for the API route
 export default function handler(req, res) {
   res.status(200).json({ text: 'Hello' });
 }
