63432e781d1452c2ff27c2c0a24e7ed63716c98e
CyberEngelbert10 <malamboengelbert@gmail.com>
2025-04-17 23:51:53 +0200

    Included 'export' in next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', // Or 'standalone' depending on your needs
+  distDir: 'out', // Specify the output directory
 };
 
 if (process.env.NODE_ENV === 'development') {
